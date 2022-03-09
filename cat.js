aba285f6aadcfb89467d55112ce5890db07bd528
utalmighty <utalmighty@gmail.com>
2022-03-09 14:02:52 +0530

    Enumerating the file

--- cat.js
+++ cat.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-// Content of file: node cat.js -[option][filepath]
+// Content of file: node cat.js -[option] [filepath]
 // OPTIONS
 //-r: read
 //-s: removes extra spaces
@@ -19,13 +19,27 @@ readCommand(command.splice(2));
 
 function readCommand(command){
     if(command[0] == "-r") {
+        // Read the file
         filepaths = command.slice(1);
         printContent(filepaths);
     }
 
     else if(command[0] == "-n") {
+        // Enumerate the lines
         filepaths = command.slice(1);
-        enumerate(filepaths);
+        for(let i=0; i<filepaths.length; i++){ 
+            if (fs.existsSync(filepaths[i])) enumerate(filepaths[i]);
+            else console.log(`File: ${filepaths[i]} does not exist.`);
+        }
+    }
+
+    else if(command[0] == "-b") {
+        // Enumerate the lines except empty lines
+        filepaths = command.slice(1);
+        for(let i=0; i<filepaths.length; i++){ 
+            if (fs.existsSync(filepaths[i])) enumerate(filepaths[i], space=true);
+            else console.log(`File: ${filepaths[i]} does not exist.`);
+        }
     }
 }
 
@@ -41,12 +55,15 @@ function printContent(filepaths) {
 }
 
 function enumerate(filepath, space=false) {
-    let content = fs.readFileSync(filepath);
+    let content = fs.readFileSync(filepath, "utf-8");
     let lines = content.split("\n");
     let number = 1;
     for(i=0; i<lines.length; i++){
         if(space){
-            if (lines[i].trim)
+            if (lines[i].trim() == "") {
+                console.log(lines[i]); // remove this.
+                continue;
+            }
         }
         console.log(`${number} ${lines[i]}`);
         number += 1;
